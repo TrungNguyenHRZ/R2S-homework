6ac19f09e5fb8276d6f107fe7e36f83753e2f868
TrungNT <trungnt10012k@gmail.com>
2023-06-13 21:46:22 +0700

    SS7

---
+++ index.js
@@ -0,0 +1,31 @@
+//Bài 1
+let callbacks = []
+for (let i = 0; i < 10; i++) {
+callbacks.push(function() { console.log(i) });
+}	
+callbacks[2]();
+
+
+//Bài 2
+
+const inventory = [
+    {type:   "machine", value: 5000},
+    {type:   "machine", value:  650},
+    {type:      "duck", value:   10},
+    {type: "furniture", value: 1200},
+    {type:   "machine", value:   77}
+  ]
+
+const countMachineValue = (array) => {
+    let totalValue = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].type === "machine") {
+            totalValue += array[i].value;
+        }
+    }
+    return totalValue;
+}
+
+const totalValueMachine = countMachineValue(inventory);
+console.log(totalValueMachine);
