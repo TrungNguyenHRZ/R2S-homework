c8539c7452a25a29bd0ba932c1c1968fb33146dd
TrungNT <trungnt10012k@gmail.com>
2023-06-13 22:00:58 +0700

    SS7

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ callbacks[2]();
 
 
 //Bài 2
-
 const inventory = [
     {type:   "machine", value: 5000},
     {type:   "machine", value:  650},
@@ -29,3 +28,21 @@ const countMachineValue = (array) => {
 
 const totalValueMachine = countMachineValue(inventory);
 console.log(totalValueMachine);
+
+//Bài 3
+const inventory2 = [
+    {type:   "machine", value: 5000},
+    {type:   "machine", value:  650},
+    {type:      "duck", value:   10},
+    {type: "furniture", value: 1200},
+    {type:   "machine", value:   77}
+  ]
+
+const sortInventory = (array) => {
+    let newArray = [];
+    array.sort((a, b) => a.value - b.value)
+    newArray.push(array);
+    return newArray;
+}
+const vvvv = sortInventory(inventory2)
+console.log(vvvv);
