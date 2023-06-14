345f18d80c05b27252884a466d37406cdabc9553
TrungNT <trungnt10012k@gmail.com>
2023-06-14 16:22:37 +0700

    SS7

--- index.js
+++ index.js
@@ -45,5 +45,66 @@ const sortInventory = (array) => {
     return newArray;
 }
 
-const vvvv = sortInventory(inventory2)
-console.log(vvvv);
+const sortAscendingValue = sortInventory(inventory2)
+console.log(sortAscendingValue);
+
+//Bài 4
+const inventory3 = [
+    {type:   "machine", value: 5000},
+    {type:   "machine", value:  650},
+    {type:      "duck", value:   10},
+    {type: "furniture", value: 1200},
+    {type:   "machine", value:   77}
+  ]
+
+const sortDInventory = (array) => {
+    let newArray = [];
+    array.sort((a, b) => b.value - a.value)
+    newArray.push(array);
+    return newArray;
+}
+
+const sortDescendingValue = sortDInventory(inventory3)
+console.log(sortDescendingValue);
+
+//Bài 5
+function go(options) {
+    let {speed = 4,
+         enable: {hyperdrive = false,
+                  frobnifier = true}} = options
+    console.log("speed=", speed,
+                "hyperdrive:", hyperdrive,
+                "frobnifier:", frobnifier)
+  }
+  const optionsParam = {
+    enable: {
+        hyperdrive: false,
+        frobnifier: false
+    }
+  };
+  go(optionsParam)
+
+  //Bài 6
+  const users = [
+    { id: 1, admin: false },
+    { id: 2, admin: false },
+    { id: 3, admin: true }
+  ];
+  
+  const displayAdmin = (users) => {
+    users.filter((user) => {
+        if(user.admin === true) {
+            console.log(user.id);
+            return true;
+        }
+        return false;
+    });
+  }
+
+  displayAdmin(users)
+  
+  //Bài 8
+  function sum(a = 0, b = 0) {
+    return a + b;
+  }
+  console.log(sum());
