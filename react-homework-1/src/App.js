fab9cfa8fd1767e0c66ccae46e5d192f77e359fa
TrungNT <trungnt10012k@gmail.com>
2023-06-15 22:40:53 +0700

    SS8

---
+++ react-homework-1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Table } from './table';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bảng Thông Tin</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-homework-1/src/table.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./style.css";
+
+export function Table() {
+    return (
+        <table className="custom-table">
+            <tr>
+                <th>Tên</th>
+                <th>Tuổi</th>
+                <th>Quốc gia</th>
+                <th>Gia đình</th>
+                <th>Ngày sinh</th>
+                <th>Giới tính</th>
+                <th>Ghi chú</th>
+            </tr>
+            <tr>
+                <td>Nguyễn Văn A</td>
+                <td>25</td>
+                <td>Việt Nam</td>
+                <td>Không</td>
+                <td>1998-05-10</td>
+                <td>Nam</td>
+                <td>Ghi chú 1</td>
+            </tr>
+            <tr>
+                <td>Nguyễn Văn A</td>
+                <td>25</td>
+                <td>Việt Nam</td>
+                <td>Không</td>
+                <td>1998-05-10</td>
+                <td>Nam</td>
+                <td>Ghi chú 1</td>
+            </tr>
+            <tr>
+                <td>Nguyễn Văn A</td>
+                <td>25</td>
+                <td>Việt Nam</td>
+                <td>Không</td>
+                <td>1998-05-10</td>
+                <td>Nam</td>
+                <td>Ghi chú 1</td>
+            </tr>
+            <tr>
+                <td>Nguyễn Văn A</td>
+                <td>25</td>
+                <td>Việt Nam</td>
+                <td>Không</td>
+                <td>1998-05-10</td>
+                <td>Nam</td>
+                <td>Ghi chú 1</td>
+            </tr>
+        </table>
+    );
+}
+
